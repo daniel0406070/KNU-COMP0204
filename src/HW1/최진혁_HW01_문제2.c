e7e770d377eb0f26b7c941501e78eb932e3f8ace
daniel040607 <daniel0406070@gmail.com>
2023-09-19 02:25:05 +0900

    hw01 update

--- src/HW1/최진혁_HW01_문제2.c
+++ src/HW1/최진혁_HW01_문제2.c
@@ -4,38 +4,18 @@ int main(){
     int num;
     printf("정수를 입력하세요: ");
     scanf("%d", &num);
-    int rvs = 0;
-    if(num){
-        rvs *= 10;
-        rvs += num % 10;
-        num /= 10;
-    } 
-    if(num){
-        rvs *= 10;
-        rvs += num % 10;
-        num /= 10;
-    } 
-    if(num){
-        rvs *= 10;
-        rvs += num % 10;
-        num /= 10;
-    } 
-    if(num){
-        rvs *= 10;
-        rvs += num % 10;
-        num /= 10;
-    } 
-    if(num){
-        rvs *= 10;
-        rvs += num % 10;
-        num /= 10;
-    } 
-    if(num){
-        rvs *= 10;
-        rvs += num % 10;
-        num /= 10;
-    } 
-    
+    printf("해당 정수를 뒤집으면 ");
+    printf("%d", num % 10);
+    num /= 10;
+    printf("%d", num % 10);
+    num /= 10;
+    printf("%d", num % 10);
+    num /= 10;
+    printf("%d", num % 10);
+    num /= 10;
+    printf("%d", num % 10);
+    num /= 10;
+    printf("%d", num % 10);
 
-    printf("해당 정수를 뒤집으면 %d\n", rvs);
+    
 }
